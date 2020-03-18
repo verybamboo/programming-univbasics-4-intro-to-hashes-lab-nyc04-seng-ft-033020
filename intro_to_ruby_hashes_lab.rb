f62fcf6926911f42d834c430240b2701fa59eaa0
George Chen <georgechen1338@gmail.com>
2020-03-18 21:41:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,8 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  my_hash = { :a => "A", :b => "B"}
-  my_hash[:a]
+  my_hash = { "a" => "A", "b" => "B"}
+  my_hash[a]
 end
 
 def pioneer
