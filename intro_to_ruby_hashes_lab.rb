3b0795a4cfbfd1c1571c58446bab1a3dd8180285
George Chen <georgechen1338@gmail.com>
2020-03-18 22:16:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,8 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   new_hash = {}
-  new_hash[:a] = 
+  new_hash[:a] = 'a'
+  new_hash
 end
 
 def read_from_hash(hash, key)
